31d6e3f0f72259bcf316b9b1548eb85696086c2a
Jordan Mossing <jordan.mossing@gmail.com>
2021-02-17 21:40:07 -0500

    updated markdown. License attempts unsuccessful. Going without

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = (license) =>
-  license !== "none" ?
-  `![Github license](https://img.shields.io/badge/license-${(encodeURI(license))}-brightgreen.svg)`:"";
+function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-//function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-//function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
 
 return `# Project Title: ${data.title}
@@ -39,7 +29,7 @@ return `# Project Title: ${data.title}
  ${data.usage}
 
  ## License:
- ${renderLicenseBadge(answers.license)}
+ ${renderLicenseSection(data.license)}
 
  ## Contributing:
  ${data.contribution} 
@@ -57,7 +47,8 @@ Reach out to me on my GitHub page at the following Link:
 
  # ![Image](./READMEtesting.PNG)
 
- # [Vimeo Link](https://vimeo.com/513644649/719843b682)
+ # [Vimeo Link](https://vimeo.com/513644649/719843b682) 
+ # Description: A link to a brief walkthrough video that demonstrates the functionality of the README generator, which will take you from start to finish. 
 
 `;
 }
